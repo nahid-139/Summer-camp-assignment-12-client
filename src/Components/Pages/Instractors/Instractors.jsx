eb9bff8b2a04996277c68257997302fa5318ec03
nahid139 <nahidnhptake@gmail.com>
2023-06-09 05:43:23 -0700

    Instractor Data is Loaded

--- src/Components/Pages/Instractors/Instractors.jsx
+++ src/Components/Pages/Instractors/Instractors.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Instractors = () => {
+
+    const [instractors ,setInstractors] =useState([]);
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data =>{
+            setInstractors(data)})
+    },[])
+    console.log(instractors);
     return (
         <div>
-            <h1>Hi I am your Instractor.</h1>
+            <h1 className='text-center font-bold text-3xl mt-10 mb-4'> Hi I am your Instractor.</h1>
+
+            <div>
+            {
+                instractors.map(item =><div
+                key={item._id}
+                
+                >
+                     <h1>name is{item.instructor.name}</h1>
 
 
-            
+                </div>)
+            }
+            </div>
+
         </div>
     );
 };
