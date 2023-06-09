e4fc2955c25e4ff6584fc196cb1635804fc47036
nahid139 <nahidnhptake@gmail.com>
2023-06-09 10:50:34 -0700

    0 avialble bg red added

--- src/Components/Pages/Classes/Classes.jsx
+++ src/Components/Pages/Classes/Classes.jsx
@@ -40,11 +40,19 @@ const Classes = () => {
                 
                 >
 
+                   
+                     
 
+                     {item?.availableSeats===0 ? (<><div className='bg-red-600'>
                      <h1>name is{item.name}</h1>
                      <h1>Aviaable Seats {item.availableSeats}</h1>
-
                      {user?.email ? (<Link to='/'><button  disabled={item.availableSeats === 0} className='bg-slate-700 text-white px-6 py-2 mt-4'>Select Course</button></Link>) : (<><Link><button  onClick={handleClick}  className='bg-slate-700 text-white px-6 py-2 mt-4'>Select Course</button></Link></>)}
+                     </div></>) : (<><div>
+                     <h1>name is{item.name}</h1>
+                     <h1>Aviaable Seats {item.availableSeats}</h1>
+                     {user?.email ? (<Link to='/'><button  disabled={item.availableSeats === 0} className='bg-slate-700 text-white px-6 py-2 mt-4'>Select Course</button></Link>) : (<><Link><button  onClick={handleClick}  className='bg-slate-700 text-white px-6 py-2 mt-4'>Select Course</button></Link></>)}
+                     </div></>)}
+                     
 
                      
 
