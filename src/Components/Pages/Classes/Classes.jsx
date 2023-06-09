a91607b3318ee314ae11d1458296419f096e5ca6
nahid139 <nahidnhptake@gmail.com>
2023-06-09 07:57:23 -0700

    Button disable req added

--- src/Components/Pages/Classes/Classes.jsx
+++ src/Components/Pages/Classes/Classes.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Classes = () => {
+   
     const [classes ,setClasses] =useState([]);
     useEffect(()=>{
         fetch('data.json')
@@ -9,23 +11,27 @@ const Classes = () => {
             setClasses(data)})
     },[])
     console.log(classes);
+
+
     return (
         <div>
             <h1 className='text-center font-bold text-2xl mt-10 mb-4'>Hi Welcome Our classes.</h1>
 
-            <div>
+            <div className='grid grid-cols-2 text-center'>
             {
-                classes.map(item =><div
+                classes.map(item =><div className=' border border-lime-200 mx-6 my-6'
                 key={item._id}
                 
                 >
                      <h1>name is{item.name}</h1>
+                     <h1>Aviaable Seats {item.availableSeats}</h1>
+                     <Link to='/'><button  disabled={item.availableSeats === 0} className='bg-slate-700 text-white px-6 py-2 mt-4'>Select Course</button></Link>
 
 
                 </div>)
             }
             </div>
-
+            
 
         </div>
     );
